cbdbd74c257803e7ce97194678a6bbce0e7d8bd3
sillygir1 <25554397+sillygir1@users.noreply.github.com>
2024-09-01 03:50:56 +0400

    Add ISO 14443-A card simulation function.

--- client/src/lvgl-gui/views/hf14a_card_view.c
+++ client/src/lvgl-gui/views/hf14a_card_view.c
@@ -12,6 +12,40 @@ static lv_obj_t *label;
 static lv_obj_t *list;
 static CardData *card_data;
 
+static void simulate_uid() {
+  iso14a_card_select_t *card = card_data->card;
+  struct {
+    uint8_t tagtype;
+    uint16_t flags;
+    uint8_t uid[10];
+    uint8_t exitAfter;
+  } PACKED payload;
+
+  // TODO: determine tag type
+  payload.tagtype = MTCLASSIC;
+
+  payload.flags = 0;
+  switch (card->uidlen) {
+  case 10:
+    payload.flags |= FLAG_10B_UID_IN_DATA;
+    break;
+  case 7:
+    payload.flags |= FLAG_7B_UID_IN_DATA;
+    break;
+  case 4:
+    payload.flags |= FLAG_4B_UID_IN_DATA;
+    break;
+  default:
+    printf("Uid length error\n");
+    return;
+  }
+  memcpy(payload.uid, card->uid, card->uidlen);
+
+  clearCommandBuffer();
+  SendCommandNG(CMD_HF_ISO14443A_SIMULATE, (uint8_t *)&payload,
+                sizeof(payload));
+}
+
 static void event_handler(lv_event_t *e) {
   lv_event_code_t code = lv_event_get_code(e);
   lv_obj_t *obj = lv_event_get_target(e);
@@ -22,6 +56,7 @@ static void event_handler(lv_event_t *e) {
     lv_obj_t *chb;
     if (strcmp(button_text, "Simulate") == 0) {
       // Simulate
+      simulate_uid();
     } else if (strcmp(button_text, "Save") == 0) {
       // Save
       fs_save_card(card_data->card, TYPE_ISO14443A);
@@ -62,12 +97,13 @@ void hf14a_card_init(void *_view_manager, void *ctx) {
 
   lv_obj_t *btn;
 
-  btn = lv_list_add_btn(list, LV_SYMBOL_LIST, "Simulate");
-  lv_obj_add_event_cb(btn, event_handler, LV_EVENT_ALL, view_manager);
   if (card_data->prev_view == VIEW_HF14AREAD) {
-    btn = lv_list_add_btn(list, LV_SYMBOL_LIST, "Save");
+    btn = lv_list_add_btn(list, LV_SYMBOL_SAVE, "Save");
     lv_obj_add_event_cb(btn, event_handler, LV_EVENT_ALL, view_manager);
   }
+  btn = lv_list_add_btn(list, LV_SYMBOL_LIST, "Simulate");
+  lv_obj_add_event_cb(btn, event_handler, LV_EVENT_ALL, view_manager);
+
   label = lv_label_create(view_manager->obj_parent);
   lv_obj_set_size(label, 220, 200);
   lv_obj_set_style_text_font(label, &lv_font_montserrat_16, LV_PART_MAIN);
