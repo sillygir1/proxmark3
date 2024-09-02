fe08d06923b3360f4617d290c8a25c82b08c5be3
sillygir1 <25554397+sillygir1@users.noreply.github.com>
2024-09-02 12:13:54 +0400

    Change card reading behaviour

--- client/src/lvgl-gui/views/hf14a_read_view.c
+++ client/src/lvgl-gui/views/hf14a_read_view.c
@@ -7,31 +7,49 @@
 #include <mifare.h>
 
 static lv_obj_t *list;
+static lv_timer_t *timer;
+static ViewManager *view_manager;
 bool keep_field;
 bool ecp;
 bool magsafe;
 bool skip_RATS;
+bool reading;
 
 #define OPTIONS_NUM 5
 static const char *options[OPTIONS_NUM] = {"Read", "Leave field ON", "Use ECP",
                                            "Magsafe", "Skip RATS"};
 
+static void read_timer(lv_timer_t *_timer) {
+  if (!reading) {
+    set_mode_text("ISO 14443-A read");
+    lv_timer_pause(timer);
+    return;
+  }
+  set_mode_text("Waiting the card");
+  iso14a_card_select_t *card = hf14a_read(keep_field, skip_RATS, ecp, magsafe);
+  if (card && card->uidlen > 0) {
+    CardData *card_data = malloc(sizeof(*card_data));
+    card_data->card = card;
+    card_data->prev_view = VIEW_HF14AREAD;
+    lv_timer_pause(timer);
+    view_manager_switch_view(view_manager, VIEW_HF14ACARD, card_data);
+  }
+}
+
 static void event_handler(lv_event_t *e) {
   lv_event_code_t code = lv_event_get_code(e);
   lv_obj_t *obj = lv_event_get_target(e);
-  ViewManager *view_manager = lv_event_get_user_data(e);
+  view_manager = lv_event_get_user_data(e);
 
   if (code == LV_EVENT_CLICKED) {
     const char *button_text = lv_list_get_btn_text(list, obj);
     lv_obj_t *chb;
     if (strcmp(button_text, options[0]) == 0) {
-      iso14a_card_select_t *card =
-          hf14a_read(keep_field, skip_RATS, ecp, magsafe);
-      if (card && card->uidlen > 0) {
-        CardData *card_data = malloc(sizeof(*card_data));
-        card_data->card = card;
-        card_data->prev_view = VIEW_HF14AREAD;
-        view_manager_switch_view(view_manager, VIEW_HF14ACARD, card_data);
+      if (!reading) {
+        reading = true;
+        lv_timer_resume(timer);
+      } else {
+        reading = false;
       }
     } else {
       chb = lv_obj_get_child(obj, -1);
@@ -58,6 +76,10 @@ static void event_handler(lv_event_t *e) {
 
 void hf14a_read_init(void *_view_manager, void *ctx) {
   set_mode_text("ISO 14443-A read");
+  timer = lv_timer_create(read_timer, 200, NULL);
+  lv_timer_pause(timer);
+  reading = false;
+
   ViewManager *view_manager = _view_manager;
   list = lv_list_create(view_manager->obj_parent);
   lv_obj_set_style_radius(list, 0, LV_PART_MAIN);
@@ -76,4 +98,7 @@ void hf14a_read_init(void *_view_manager, void *ctx) {
   }
 }
 
-void hf14a_read_exit() { lv_obj_del(list); }
+void hf14a_read_exit() {
+  lv_timer_del(timer);
+  lv_obj_del(list);
+}
