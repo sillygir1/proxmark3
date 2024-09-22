2aa0ca8f831299cd16713d6e9a8cd3fba5280547
sillygir1 <25554397+sillygir1@users.noreply.github.com>
2024-09-22 17:07:24 +0400

    Update makefile and build script

--- make.sh
+++ make.sh
@@ -7,7 +7,7 @@ export LZ4_PATH=$DUO_BUILDROOT_SDK/buildroot-2021.05/output/milkv-duo256m-sd_mus
 export BZIP2_PATH=$DUO_BUILDROOT_SDK/buildroot-2021.05/output/milkv-duo256m-sd_musl_riscv64/build/bzip2-1.0.8/
 
 if [[ $1 == "client" ]]; then
-    make clean && CFLAGS="-L$LZ4_PATH -I$LZ4_PATH -L$BZIP2_PATH -I$BZIP2_PATH -I$DUO_SDK/rootfs/usr/include -I$PILK_SOFTWARE/lvgl/ -I$PILK_SOFTWARE/lvgl-launcher/src/ -I$PILK_SOFTWARE/duo-battery -I$PILK_SOFTWARE/duo-encoder-kb -I$PILK_SOFTWARE/view-manager -I$PILK_SOFTWARE/storage -I$PILK_SOFTWARE/file-manager" make client cpu_arch=generic SKIPREVENGTEST=1 SKIPWHEREAMISYSTEM=1 SKIPBT=1 SKIPQT=1 SKIPREADLINE=1 SKIPLINENOISE=1 SKIPLUASYSTEM=1 SKIPJANSSONSYSTEM=1 SKIPPYTHON=1 SKIPGD=1 -j
+    make clean && CFLAGS="-L$LZ4_PATH -I$LZ4_PATH -L$BZIP2_PATH -I$BZIP2_PATH -I$DUO_SDK/rootfs/usr/include -I$PILK_SOFTWARE/lvgl/ -I$PILK_SOFTWARE/duo-battery -I$PILK_SOFTWARE/duo-encoder-kb -I$PILK_SOFTWARE/view-manager -I$PILK_SOFTWARE/storage -I$PILK_SOFTWARE/file-manager" make client cpu_arch=generic SKIPREVENGTEST=1 SKIPWHEREAMISYSTEM=1 SKIPBT=1 SKIPQT=1 SKIPREADLINE=1 SKIPLINENOISE=1 SKIPLUASYSTEM=1 SKIPJANSSONSYSTEM=1 SKIPPYTHON=1 SKIPGD=1 -j
     elif [[ $1 == "image" ]]; then
     make clean && make bootrom/install && IMAGE=1 make fullimage/install SKIPQT=1 -j
 else
